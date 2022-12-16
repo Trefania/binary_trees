1c8d36963d8e6ebc0e9840e1eab395c2f5263759
Trefania <107484364+Trefania@users.noreply.github.com>
2022-12-16 19:51:44 +0200

    traversal

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+#include "10-binary_tree_depth.c"
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: is a pointer to the first node
+ * @second: is a pointer to the second node
+ * Return: return a pointer to the lowest common ancestor node of the
+ * two given nodes
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									 const binary_tree_t *second)
+{
+	int first_depth, second_depth;
+
+	if (!first || !second)
+		return (NULL);
+	first_depth = binary_tree_depth(first);
+	second_depth = binary_tree_depth(second);
+	while (first_depth > second_depth)
+	{
+		first = first->parent;
+		first_depth--;
+	}
+	while (second_depth > first_depth)
+	{
+		second = second->parent;
+		second_depth--;
+	}
+	while ((second->parent && first->parent) && (first != second))
+	{
+		first = first->parent;
+		second = second->parent;
+	}
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	return (NULL);
+}
--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,22 +1,45 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
+int height(const binary_tree_t *tree);
 /**
- * binary_tree_balance - function that measures the
- * balance factor of a binary tree
- * @tree: a pointer to the root node of the tree to measure
- * the balance factor
- *
- * Return: balanced factor
+ * binary_tree_balance - measure balance of a tree
+ * @tree: pointer
+ * Return: balance count difference
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int right = 0, left = 0, total = 0;
+	int l_height = 0;
+	int r_height = 0;
 
-	if (tree)
-	{
-		left = ((int)binary_tree_height(tree->left));
-		right = ((int)binary_tree_height(tree->right));
-		total = left - right;
-	}
-	return (total);
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		l_height = height(tree->left);
+	if (tree->right)
+		r_height = height(tree->right);
+
+	return (l_height - r_height);
+}
+
+/**
+ * height - Measures height of binary tree
+ * @tree: pointer to root
+ * Return: int height
+ */
+
+int height(const binary_tree_t *tree)
+{
+	int r_count, l_count;
+
+	if (tree == NULL)
+		return (0);
+
+	r_count = height(tree->right);
+	l_count = height(tree->left);
+
+	if (l_count > r_count)
+		return (l_count + 1);
+	else
+		return (r_count + 1);
 }
